faf1c09d06821f80175a2cb758964dd323b13800
yongkai teo <yongkaiteo@yongkais-MacBook-Pro.local>
2016-08-04 18:00:20 +0800

    Add test case

---
+++ test/cli_test.exs
@@ -0,0 +1,11 @@
+defmodule CliTest do
+  use ExUnit.Case
+  doctest Issues
+
+  import(Issues.CLI, only: [parse_args: 1])
+
+  test ":help returned by option parsing with -h and --help options" do
+    assert(parse_args(["-h", "anything"]) == :help)
+    assert(parse_args(["--help", "anything"]) == :help)
+  end
+end
