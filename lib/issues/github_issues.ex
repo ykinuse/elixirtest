a43839d1e33ba9fa04fddbb9481d95a359e809f3
Kai <ykinuse@gmail.com>
2016-08-04 23:33:58 +0800

    Add parser

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,21 @@
+defmodule Issues.GithubIssues do
+  @user_agent  [ {"User-agent", "Elixir dave@pragprog.com"} ]
+
+  def fetch(user, project) do
+   issues_url(user, project)
+   |> HTTPoison.get(@user_agent)
+   |> handle_response
+  end
+
+  def issues_url(user, project) do
+   "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({ :ok, %{status_code: 200, body: body}}) do
+   { :ok,    Poison.Parser.parse!(body) }
+  end
+
+  def handle_response({ ___, %{status_code: ___, body: body}}) do
+   { :error, Poison.Parser.parse!(body) }
+  end
+end
