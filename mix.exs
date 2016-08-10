09b498509573cb6b4879c833382a5e89c58df809
yongkai teo <yongkaiteo@yongkais-MacBook-Pro.local>
2016-08-10 18:21:50 +0800

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -2,15 +2,17 @@ defmodule Issues.Mixfile do
   use Mix.Project
 
   def project do
-    [ app:             :issues,
-      escript:         escript_config,
-      version:         "0.0.1",
-      elixir:          "~> 1.2",
-      build_embedded:  Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
-      deps:            deps ]
+    [ app:              :issues,
+      escript:          escript_config,
+      version:          "0.0.1",
+      elixir:           "~> 1.2",
+      build_embedded:   Mix.env == :prod,
+      start_permanent:  Mix.env == :prod,
+      name:             "Issues",
+      source_url:       "https://github.com/pragdave/issues",
+      deps:             deps ]
   end
-  
+
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
@@ -35,8 +37,10 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.8"},
-      {:poison,    "~> 1.5"}
+      httpoison:  "~> 0.8",
+      poison:    "~> 1.5",
+      ex_doc:     "~> 0.11",
+      earmark:    ">= 0.0.0"
     ]
   end
 
